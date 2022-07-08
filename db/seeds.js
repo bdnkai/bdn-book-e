bc50bf74d651b2f1b8751fc61291dc2e73312179
bdnkai <bdn.kai@gmail.com>
2022-07-07 17:15:49 -0700

    refactor ss

--- db/seeds.js
+++ db/seeds.js
@@ -2,19 +2,19 @@ const mongoose = require('./connection');
 
 const Bookmark = require('../models/bookmark');
 const User = require('../models/user');
-const bookmarkseeds = require('./seeds.json');
+const bookmarkSeeds = require('./seeds.json');
 
 Bookmark.deleteMany({})
-  .then(() => User.deleteMany({}))
-  .then(() => {
-    return User.create({ email: 'fake@email.com', name: 'Fake Person' })
-      .then((user) =>
-        bookmarkseeds.map((bookmark) => ({ ...bookmark, owner: user._id }))
-      )
-      .then((bookmarks) => Bookmark.insertMany(bookmarks));
-  })
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => {
-    process.exit();
-  });
+	.then(() => User.deleteMany({}))
+	.then(() => {
+		return User.create({ email: 'fake@email.com', name: 'Fake Person' })
+			.then((user) =>
+				bookmarkSeeds.map((bookmark) => ({ ...bookmark, owner: user._id }))
+			)
+			.then((bookmarks) => Bookmark.insertMany(bookmarks));
+	})
+	.then(console.log)
+	.catch(console.error)
+	.finally(() => {
+		process.exit();
+	});
